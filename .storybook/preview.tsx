9d9a0410354eeba5715994fe8b05c1e20ca4e95f
Dan Atanasov <danmanm@gmail.com>
2025-10-08 11:09:33 +0300

    Apply global styles to Storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,20 @@
+import type { Preview } from "@storybook/nextjs";
+import { Noto_Serif } from "next/font/google";
+import "../app/globals.css";
+
+const notoSerif = Noto_Serif({
+  variable: "--font-noto-serif",
+  subsets: ["latin"],
+});
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <body className={`${notoSerif.variable} antialiased`}>
+        <Story />
+      </body>
+    ),
+  ],
+};
+
+export default preview;
