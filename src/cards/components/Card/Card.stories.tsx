5d6194a87ad20f7b82cbc24c357ab0c665c87a39
Dan Atanasov <danmanm@gmail.com>
2026-06-27 09:31:01 +0300

    feat: add CardsGallery story to showcase all card bases

--- src/cards/components/Card/Card.stories.tsx
+++ src/cards/components/Card/Card.stories.tsx
@@ -18,4 +18,23 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
+export const CardsGallery: Story = {
+  render: () => (
+    <div className="flex gap-2 flex-wrap">
+      {Object.entries(cardBases).map(([id, card]) => (
+        <div key={id} className="flex flex-col gap-2">
+          <Card card={card} />
+          <div className="text-center">{id}</div>
+        </div>
+      ))}
+    </div>
+  ),
+}
+
 export const Character: Story = {}
+
+export const Instance: Story = {
+  args: {
+    card: cardBases['bookOfAsh'],
+  },
+}
